4e10bc30fc2c5346065a818d3e53bc5902f6ede6
Allene Norton <norton.allene@gmail.com>
2021-12-13 15:08:59 -0600

    add controller

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+
+//Controllers
+app.use('/places', require('./controllers/places-control'))
+
+//Root
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+//Wildcard
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
