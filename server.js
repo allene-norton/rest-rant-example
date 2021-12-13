be66bd922ac5ebf56c19e5968443e858bf6647e7
Allene Norton <norton.allene@gmail.com>
2021-12-13 15:04:56 -0600

    set up server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Munchin' on port ${port}...`)
+})
