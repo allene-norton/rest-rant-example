6f4d0c75894a505677b4a1bbdec4eeb9352426ec
Allene Norton <norton.allene@gmail.com>
2021-12-13 20:22:16 -0600

    put and delete are working

--- controllers/places-control.js
+++ controllers/places-control.js
@@ -46,20 +46,64 @@ router.get('/:id', (req, res) => {
 
 })
 
+//edit
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id], id: req.params.id })
+    }
+})
+
+//update
+router.put('/:id', (req, res) => {
+    console.log(req.body)
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        console.log(req.body)
+        // Save the new data into places[id]
+        places[req.params.id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+  })
+
 //Delete
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
-      res.render('error404')
+        res.render('error404')
     }
     else if (!places[id]) {
-      res.render('error404')
+        res.render('error404')
     }
     else {
-      places.splice(id, 1)
-      res.redirect('/places')
+        places.splice(id, 1)
+        res.redirect('/places')
     }
-  })
+})
 
 
 module.exports = router
